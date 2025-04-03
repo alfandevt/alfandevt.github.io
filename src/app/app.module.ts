a53ff1192645f3a4a68b2643bcfd880e5b52e7c9
Aldiansyah Fandevtian <fandevtian@gmail.com>
2025-04-03 11:42:07 +0700

    add init image-show component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ImageShowComponent } from './components/image-show/image-show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ImageShowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/image-show/image-show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-show',
+  standalone: false,
+  templateUrl: './image-show.component.html',
+  styleUrl: './image-show.component.scss'
+})
+export class ImageShowComponent {
+
+}
